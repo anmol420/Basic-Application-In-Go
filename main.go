f9b8e3f7c5b5bd277d85280ab6b16fa4397a26b1
Slayer_op <76034132+anmolrockz@users.noreply.github.com>
2022-01-21 14:18:17 +0530

    Initial Files

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "Hello World!")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number Of Bytes Written : %d",n))
+	})
+
+	http.ListenAndServe(":3000", nil)
+}
