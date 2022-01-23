3d535f0b7843fe914ef2f206dcb77e77d329f453
Slayer_op <76034132+anmolrockz@users.noreply.github.com>
2022-01-23 14:18:41 +0530

    Added Baic Template

--- main.go
+++ main.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+const portnumber = ":3000"
+
+func home(w http.ResponseWriter, r *http.Request) {
+	renderTemp(w, "home.page.tmpl")
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	renderTemp(w, "about.page.tmpl")
+}
+
+func renderTemp(w http.ResponseWriter, tmpl string) {
+	parsedTemp, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemp.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error occured", err)
+		return
+	}
+}
+
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello World!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number Of Bytes Written : %d",n))
-	})
+	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
 
-	http.ListenAndServe(":3000", nil)
+	fmt.Println(fmt.Sprintf("Starting The Application On Port %s", portnumber))
+	
+	http.ListenAndServe(portnumber, nil)
 }
