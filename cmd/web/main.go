9a67e65ca427fdd5e329d4c93802a58d45b7ba7e
Slayer_op <76034132+anmolrockz@users.noreply.github.com>
2022-01-25 13:46:58 +0530

    Reconstruction Of Code

---
+++ cmd/web/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"myapp/pkg/handlers"
+	"net/http"
+)
+
+const portnumber = ":3000"
+
+func main() {
+
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Println(fmt.Sprintf("Starting The Application On Port %s", portnumber))
+	
+	http.ListenAndServe(portnumber, nil)
+}
